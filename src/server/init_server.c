be11bd07c8c44b1eba6f6030a0304c5b1a21de53
Enzo VALLET <enzo1.vallet@epitech.eu>
2022-06-09 16:46:51 +0200

    commit 2

--- src/server/init_server.c
+++ src/server/init_server.c
@@ -15,19 +15,19 @@ server_t *init_server(char *ip_addr, int port)
     int opt = 1;
 
     if (sockfd == -1)
-        exit (79);
+        exit (84);
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-        exit (80);
+        exit (84);
     server->ip_addr = ip_addr;
     server->port = port;
     server->addr.sin_addr.s_addr = inet_addr(ip_addr);
     server->addr.sin_family = AF_INET;
     server->addr.sin_port = htons(port);
     if (bind(sockfd,(struct sockaddr *)&server->addr, sizeof(server->addr)) == -1)
-        exit (81);
+        exit (84);
     if (listen(sockfd, 3) == -1)
-        exit (82);
+        exit (84);
     if ((server->socket_client = accept(sockfd, (struct sockaddr *)&server->addr, (socklen_t *)&addrlen)) == -1)
-        exit (83);
+        exit (84);
     return server;
 }
